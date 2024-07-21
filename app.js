54d27bb5ecb2ff9f70862a879147b8107f367700
Muhammad-Alsharafi <117654669+Muhammad-Alsharafi@users.noreply.github.com>
2024-07-22 00:40:45 +0300

    Update app.js

--- app.js
+++ app.js
@@ -6,27 +6,20 @@ document.addEventListener('DOMContentLoaded', function() {
     let isPlayingFirstVideo = true;
 
     video1.onended = function() {
-        video1.pause();
-        video1.currentTime = 0;
-        video2.style.display = 'block';
-        video2.play();
-        audio.play();
-        displayText.textContent = "Happy now? She is crying";
-        isPlayingFirstVideo = false;
+        video1.play(); // Ensure video1 keeps looping
     };
 
-    video2.onended = function() {
+    audio.onended = function() {
+        video2.style.display = 'none';
         video2.pause();
         video2.currentTime = 0;
         video1.style.display = 'block';
         video1.play();
-        audio.pause();
-        audio.currentTime = 0;
         displayText.textContent = "Don't hit the cherry";
         isPlayingFirstVideo = true;
     };
 
-    window.playSecondVideoAndSound = function() {
+    video1.onclick = function() {
         if (isPlayingFirstVideo) {
             video1.style.display = 'none';
             video2.style.display = 'block';
